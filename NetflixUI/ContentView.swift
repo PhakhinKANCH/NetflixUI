727c58475c5bbde28d819928aabbc517f7556384
Phakin <Phakhin.k@kkumail.com>
2024-04-12 11:08:21 +0700

    Initial Commit

---
+++ NetflixUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetflixUI
+//
+//  Created by Phakhin Kanch on 12/4/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetflixUI/NetflixUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixUIApp.swift
+//  NetflixUI
+//
+//  Created by Phakhin Kanch on 12/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct NetflixUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
